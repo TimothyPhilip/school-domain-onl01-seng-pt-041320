e028ad1f4900bc365d799cfb2874053521a56386
Philippe Timothe <philippetimothe@gmail.com>
2020-04-30 12:35:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  
+  attr_accessor :grade
+  attr_reader :school, :roster, :add_student
+
+  
+  def initialize(school, roster = {}) 
+    @school = school 
+    @roster = roster
+  end 
+    
+  def add_student(name, grade)
+    if roster.key?(grade) == true
+        @roster[grade] << name
+    else
+        roster[grade] = []
+        roster[grade] << name
+    end
+  end
+    
+end 
